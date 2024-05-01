09a73aa0ad0899abbf807750aadcd8161ab96ba7
melani-depetris <melani.md98@gmail.com>
2024-04-30 23:06:30 -0300

    add NavBar

---
+++ public/svg/arrow-drop-menu.jsx
@@ -0,0 +1,7 @@
+
+export function ArrowDropMenu() {
+  return (
+  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path fill="#000000" d="m12 15l-5-5h10z"/></svg>
+
+  );
+}
