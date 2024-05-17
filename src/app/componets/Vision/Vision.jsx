97ea8d6ef4c5c44f5a6ab8bafafd806957f05b6b
FausUCU <88690038+FausUCU@users.noreply.github.com>
2024-05-17 12:55:06 -0300

    Página Fundación
    Seccion de vision y objetivos agregada a pagina de fundacion

---
+++ src/app/componets/Vision/Vision.jsx
@@ -0,0 +1,14 @@
+export const Vision = () => {
+    return (
+        <div className="flex flex-col mx-auto py-8 bg-colorTerciario">
+            <div className="flex flex-col w-[80%] mx-auto py-8">
+                <h3 className="text-blackHtas font-bold text-2xl mb-6" id="Vision">VISIÓN</h3>
+                <p className="text-blackHtas">
+                Queremos ser reconocidos por nuestra capacidad para transformar vidas, tanto de los animales rescatados como de las personas involucradas en nuestra misión, inspirando un cambio positivo en la sociedad.  Buscamos impactar positivamente en nuestra comunidad, fomentando una cultura de compasión y empatía hacia los animales, donde cada individuo se sienta responsable de su bienestar.
+                </p>
+            </div>
+        
+        </div>
+
+    );
+  }
