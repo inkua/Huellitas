f86b007e11d197c2c50dcdd60f990e7cee44c8d1
Gonzalo Vega <113808588+gevega@users.noreply.github.com>
2024-05-21 13:01:11 -0300

    Adoption page and responsive design finished (#12)
    * Secciones Hero e Historias
    
    * Banner and media queries modifications
    
    * Adopcion page added. Changes to the Hero and History section, such as elements and responsive design
    
    * Custom class for text and button added
    
    * Fix padding primary-btn
    
    * Important plugin added
    
    * Responsive design modifications
    
    * Adoption page and responsive design finished
    
    * Link to adoption page added

--- src/app/adopcion/page.jsx
+++ src/app/adopcion/page.jsx
@@ -1,14 +1,24 @@
+"use client"
 import Image from 'next/image'
 import adopcion_001 from '../../../public/assets/Adopcion/adopcion_001.jpg'
+import adopcion_001_lg from '../../../public/assets/Adopcion/adopcion_001_lg.jpg'
+import { useState } from "react"
+import FAQ_Adopcion from '../componets/FAQ_Adopcion/FAQ_Adopcion'
 
 function Adopcion() {
+    const [info, setInfo] = useState(false);
+
+    const viewInfo = () => {
+        setInfo(!info);
+    }
+
     return (
         <section>
-            <div className='w-full h-72 lg:bg-indigo-100'>
+            <div className='w-full h-72 lg:bg-tertiaryColor'>
                 <div className="flex w-full h-full max-w-screen-2xl mx-auto items-end lg:justify-center lg:items-center p-8 lg:p-0 lg:py-16 bg-adoption-banner lg:bg-none bg-cover bg-opacity-80">
                     <div className="flex flex-col w-full lg:w-9/12 gap-8">
-                        <h1 className="font-bold text-4xl lg:text-3xl text-white lg:text-black">ADOPTALOS</h1>
-                        <p className="hidden lg:block text-justify text-black">
+                        <h1 className="font-bold heading-1">ADOPTALOS</h1>
+                        <p className="hidden lg:block font-normal par-3">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et doloret.
                         </p>
                     </div>
@@ -17,70 +27,92 @@ function Adopcion() {
 
             <div className="flex w-full max-w-screen-2xl mx-auto justify-center items-center py-16">
                 <div className="flex flex-col w-full lg:w-9/12 gap-y-20">
-                    <h1 className="hidden lg:block font-bold text-3xl text-black">BUSCAMOS UN HOGAR CON AMOR</h1>
+                    <h1 className="hidden lg:block font-bold heading-1">BUSCAMOS UN HOGAR CON AMOR</h1>
                     <div className="grid grid-cols-2 lg:grid-cols-3 md:grid-cols-3 px-4 gap-x-4 lg:gap-x-16 gap-y-20">
                         
                         <div className="flex flex-col gap-4 items-center lg:gap-8">
                             <div className='w-full overflow-hidden rounded-full'>
                                 <Image src={adopcion_001} alt='Foto del perrito' width={100} height={100} className='flex w-full'></Image>
                             </div>
-                            <h1 className='font-bold text-xl text-black'>MAX - 3 AÑOS</h1>
-                            <button type="button" className="w-full py-6 lg:py-1 md:py-1 text-white bg-violetHtas hover:bg-pinkHtas rounded-full shadow-md shadow-grey-400">
-                            SABER MÁS
-                            </button>
+                            <h1 className='font-bold par-2'>MAX - 3 AÑOS</h1>
+                            <button type="button" onClick={() => viewInfo()} className="w-full primary-btn">SABER MÁS</button>
                         </div>
 
                         <div className="flex flex-col gap-4 items-center lg:gap-8">
                             <div className='w-full overflow-hidden rounded-full'>
                                 <Image src={adopcion_001} alt='Foto del perrito' width={100} height={100} className='flex w-full'></Image>
                             </div>
-                            <h1 className='font-bold text-xl text-black'>MAX - 3 AÑOS</h1>
-                            <button type="button" className="w-full py-6 lg:py-1 md:py-1 text-white bg-violetHtas hover:bg-pinkHtas rounded-full shadow-md shadow-grey-400">
-                            SABER MÁS
-                            </button>
+                            <h1 className='font-bold par-2'>MAX - 3 AÑOS</h1>
+                            <button type="button" onClick={() => viewInfo()} className="w-full primary-btn">SABER MÁS</button>
                         </div>
 
                         <div className="flex flex-col gap-4 items-center lg:gap-8">
                             <div className='w-full overflow-hidden rounded-full'>
                                 <Image src={adopcion_001} alt='Foto del perrito' width={100} height={100} className='flex w-full'></Image>
                             </div>
-                            <h1 className='font-bold text-xl text-black'>MAX - 3 AÑOS</h1>
-                            <button type="button" className="w-full py-6 lg:py-1 md:py-1 text-white bg-violetHtas hover:bg-pinkHtas rounded-full shadow-md shadow-grey-400">
-                            SABER MÁS
-                            </button>
+                            <h1 className='font-bold par-2'>MAX - 3 AÑOS</h1>
+                            <button type="button" onClick={() => viewInfo()} className="w-full primary-btn">SABER MÁS</button>
                         </div>
 
                         <div className="flex flex-col gap-4 items-center lg:gap-8">
                             <div className='w-full overflow-hidden rounded-full'>
                                 <Image src={adopcion_001} alt='Foto del perrito' width={100} height={100} className='flex w-full'></Image>
                             </div>
-                            <h1 className='font-bold text-xl text-black'>MAX - 3 AÑOS</h1>
-                            <button type="button" className="w-full py-6 lg:py-1 md:py-1 text-white bg-violetHtas hover:bg-pinkHtas rounded-full shadow-md shadow-grey-400">
-                            SABER MÁS
-                            </button>
+                            <h1 className='font-bold par-2'>MAX - 3 AÑOS</h1>
+                            <button type="button" onClick={() => viewInfo()} className="w-full primary-btn">SABER MÁS</button>
                         </div>
 
                         <div className="flex flex-col gap-4 items-center lg:gap-8">
                             <div className='w-full overflow-hidden rounded-full'>
                                 <Image src={adopcion_001} alt='Foto del perrito' width={100} height={100} className='flex w-full'></Image>
                             </div>
-                            <h1 className='font-bold text-xl text-black'>MAX - 3 AÑOS</h1>
-                            <button type="button" className="w-full py-6 lg:py-1 md:py-1 text-white bg-violetHtas hover:bg-pinkHtas rounded-full shadow-md shadow-grey-400">
-                            SABER MÁS
-                            </button>
+                            <h1 className='font-bold par-2'>MAX - 3 AÑOS</h1>
+                            <button type="button" onClick={() => viewInfo()} className="w-full primary-btn">SABER MÁS</button>
                         </div>
 
                         <div className="flex flex-col gap-4 items-center lg:gap-8">
                             <div className='w-full overflow-hidden rounded-full'>
                                 <Image src={adopcion_001} alt='Foto del perrito' width={100} height={100} className='flex w-full'></Image>
                             </div>
-                            <h1 className='font-bold text-xl text-black'>MAX - 3 AÑOS</h1>
-                            <button type="button" className="w-full py-6 lg:py-1 md:py-1 text-white bg-violetHtas hover:bg-pinkHtas rounded-full shadow-md shadow-grey-400">
-                            SABER MÁS
-                            </button>
+                            <h1 className='font-bold par-2'>MAX - 3 AÑOS</h1>
+                            <button type="button" onClick={() => viewInfo()} className="w-full primary-btn">SABER MÁS</button>
                         </div>
 
                     </div>
+                </div>
+
+                {info && 
+                    (
+                        <div className='flex fixed inset-0 justify-center items-center gap-8 backdrop-blur-sm'>
+                            <div className='grid lg:grid-cols-[25%_1fr] w-9/12 lg:gap-14 p-10 bg-white rounded-3xl shadow-lg'>
+                                <div className='flex w-full items-center'>
+                                    <Image src={adopcion_001_lg} alt='Foto del perrito' width={100} height={100} className='hidden lg:block w-full rounded-3xl'></Image>
+                                </div>
+
+                                <div className='flex flex-col justify-center items-start gap-8'>
+                                    <h1 className='font-bold heading-2'>MAX</h1>
+                                    <h1 className='font-bold par-1'>Edad: <span className='font-medium par-1'>3 años</span></h1>
+                                    <p className='font-bold par-1'>Caracteristicas: <span className='font-medium par-1'>
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore totam harum officiis voluptas sit! Facere possimus recusandae culpa, ipsum aliquid corrupti ipsam minus quos iste?
+                                        </span>
+                                    </p>
+                                    <p className='font-bold par-1'>Historia: <span className='font-medium par-1'>
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore totam harum officiis voluptas sit! Facere possimus recusandae culpa, ipsum aliquid corrupti ipsam minus quos iste?
+                                        </span>
+                                    </p>
+
+                                    <button type="button" onClick={() => viewInfo()} className="primary-btn">
+                                        QUIERO ADOPTARLO
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
+            <div className='bg-tertiaryColor py-16'>
+                <div className='max-w-screen-2xl mx-auto'>
+                    <FAQ_Adopcion />
                 </div>
             </div>
         </section>
--- src/app/componets/History/History.jsx
+++ src/app/componets/History/History.jsx
@@ -7,24 +7,24 @@ function History() {
     return (
         <section>
             <div className='flex flex-col w-full max-w-screen-2xl mx-auto justify-center items-center gap-8 py-16 bg-white'>
-                <h1 className='w-11/12 lg:w-9/12 font-bold text-center lg:text-left md:text-left text-3xl text-black'>SIGUE SUS HISTORIAS</h1>
-                <div className='grid grid-cols-[90%_90%_90%] lg:grid-cols-3 w-full lg:w-9/12 px-4 overflow-x-scroll snap-mandatory snap-x no-scrollbar gap-4'>
+                <h1 className='w-11/12 lg:w-9/12 font-bold heading-1 text-left'>SIGUE SUS HISTORIAS</h1>
+                <div className='grid overflow-x-scroll grid-cols-[repeat(3,90%)] lg:grid-cols-3 w-full lg:w-9/12 px-4 lg:px-0 snap-mandatory snap-x no-scrollbar gap-4'>
                     
                     <div className='flex flex-col gap-4 snap-center'>
                         <Image src={history_001} alt='Foto del perrito protagonista'></Image>
-                        <p className='text-black'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea cumque ad iure quaerat totam?</p>
+                        <p className='font-normal par-3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea cumque ad iure quaerat totam?</p>
                         <button className='w-fit px-4 py-1 text-sm text-black border-solid border-2 border-black rounded-2xl'>SABER MÁS...</button>
                     </div>
 
                     <div className='flex flex-col gap-4 snap-center'>
                         <Image src={history_002} alt='Foto del perrito protagonista'></Image>
-                        <p className='text-black'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea cumque ad iure quaerat totam?</p>
+                        <p className='font-normal par-3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea cumque ad iure quaerat totam?</p>
                         <button className='w-fit px-4 py-1 text-sm text-black border-solid border-2 border-black rounded-2xl'>SABER MÁS...</button>
                     </div>
 
                     <div className='flex flex-col gap-4 snap-center'>
                         <Image src={history_003} alt='Foto del perrito protagonista'></Image>
-                        <p className='text-black'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea cumque ad iure quaerat totam?</p>
+                        <p className='font-normal par-3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea cumque ad iure quaerat totam?</p>
                         <button className='w-fit px-4 py-1 text-sm text-black border-solid border-2 border-black rounded-2xl'>SABER MÁS...</button>
                     </div>
 
