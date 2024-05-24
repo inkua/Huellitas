806b34ef39b77307bb68eade18fe12bf33ed555c
FausUCU <88690038+FausUCU@users.noreply.github.com>
2024-05-24 20:23:58 -0300

    ajusteComponenetes (#14)
    Ajuste de componentes mission y vision en pagina fundacion
    para nuevos colores y letras
    
    Co-authored-by: Manuel Florez <89274119+manuel14mds@users.noreply.github.com>

--- src/app/componets/Mision/Mision.jsx
+++ src/app/componets/Mision/Mision.jsx
@@ -1,8 +1,8 @@
 export const Mision = () => {
   return (
-    <div className="flex flex-col w-[80%] mx-auto py-8 max-w-screen-2xl">
-      <h3 className="font-bold heading-1 mb-6" id="mision">MISIÓN</h3>
-      <p className="font-normal par-3">
+    <div className="flex flex-col w-[80%] mx-auto py-8">
+      <h3 className="text-primaryFont font-bold text-h1-m mb-6" id="mision">MISIÓN</h3>
+      <p className="text-primaryFont text-p1-m1">
         Nuestra misión es rescatar, rehabilitar y encontrar hogares amorosos
         para los animales en situación de abandono o maltrato, brindándoles una
         segunda oportunidad para vivir una vida plena y feliz. Queremos ser
