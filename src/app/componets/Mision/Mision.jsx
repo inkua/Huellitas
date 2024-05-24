acedeb06f73fe07f772d0602e26bf0276a27814e
FausUCU <88690038+FausUCU@users.noreply.github.com>
2024-05-24 17:20:02 -0300

    ajusteComponenetes
    Ajuste de componentes mission y vision en pagina fundacion
    para nuevos colores y letras

--- src/app/componets/Mision/Mision.jsx
+++ src/app/componets/Mision/Mision.jsx
@@ -1,8 +1,8 @@
 export const Mision = () => {
   return (
     <div className="flex flex-col w-[80%] mx-auto py-8">
-      <h3 className="text-blackHtas font-bold text-2xl mb-6" id="mision">MISIÓN</h3>
-      <p className="text-blackHtas">
+      <h3 className="text-primaryFont font-bold text-h1-m mb-6" id="mision">MISIÓN</h3>
+      <p className="text-primaryFont text-p1-m1">
         Nuestra misión es rescatar, rehabilitar y encontrar hogares amorosos
         para los animales en situación de abandono o maltrato, brindándoles una
         segunda oportunidad para vivir una vida plena y feliz. Queremos ser
