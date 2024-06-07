ea4104652aa44e5a88d07f3da0cda98a362d6d0b
Melani-Depetris <melani.md98@gmail.com>
2024-06-07 20:54:23 -0300

    Melani (#23)
    * add modal Terms And Conditions
    
    * add responsive design
    
    * add components view fundation, component nuestraHistoria y Mision
    
    * change design footer, navBar, nuestraHistoria, mision
    
    * fix responsive navBar
    
    * fix footer w-full and add links
    
    * fix design
    
    * add modals
    
    * add design navBar

---
+++ src/app/(main)/colaboracion/components/DonaServiciosContent.jsx
@@ -0,0 +1,32 @@
+const DonaServiciosContent = () => {
+  return (
+    <div>
+        <div className="py-6">
+        <h2 className="heading-2 font-semibold">ESTERILIZACIONES</h2>
+      <p className="par-3">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry.
+      </p>
+        </div>
+    
+
+<div className="py-6">
+<h2 className="heading-2 font-semibold">HOGAR DE PASO</h2>
+      <p className="par-3">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry.
+      </p>
+</div>
+
+      <div className="bg-primaryColor rounded-lg p-4">
+        <h2 className="text-h2-m lg:text-h2-w md:text-h2-w font-semibold">¿CÓMO PUEDO PRESTAR LOS SERVICIOS</h2>
+        <p className="text-p3-m lg:text-p3-w md:text-p3-w">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DonaServiciosContent;
--- src/app/(main)/colaboracion/page.jsx
+++ src/app/(main)/colaboracion/page.jsx
@@ -1,9 +1,33 @@
+'use client'
+import { useState } from 'react'
 import Card from './components/Card'
 import Image from 'next/image'
+import Modal from './components/Modal'
 
 function Colaboracion() {
+    const [modalVisible, setModalVisible] = useState({
+        donar_dinero: false,
+        donar_insumos: false,
+        donar_servicios: false
+    });
+
+    const openModal = (modalType) => {
+        setModalVisible(prevState => ({
+            ...prevState,
+            [modalType]: true
+        }));
+    };
+
+    const closeModal = () => {
+        setModalVisible({
+            donar_dinero: false,
+            donar_insumos: false,
+            donar_servicios: false
+        });
+    };
+
     return (
-        <main>
+        <main className='relative'>
             <section className='flex flex-col relative w-full h-80 lg:h-96 max-w-screen-2xl mx-auto bg-colab-banner bg-cover bg-center' role='banner'>
                 <div className='flex absolute w-full lg:w-6/12 h-full items-end lg:justify-center p-6'>
                     <div className='flex w-full lg:w-2/5 justify-center lg:justify-start items-end'>
@@ -15,9 +39,9 @@ function Colaboracion() {
             <section className='flex flex-col w-full max-w-screen-2xl mx-auto justify-center items-center gap-8 py-16 bg-white'>
                 <h1 className='w-11/12 lg:w-9/12 font-bold heading-1 text-left'>¿COMO QUIERES COLABORAR?</h1>
                 <div className='grid overflow-x-scroll grid-cols-[repeat(3,45%)] lg:grid-cols-3 w-full lg:w-9/12 px-4 lg:px-0 snap-mandatory snap-x no-scrollbar gap-4' role='list'>
-                    <Card source={'/assets/Colaboracion/colaboracion_001.jpg'} btn_txt={'DONAR DINERO'}/>
-                    <Card source={'/assets/Colaboracion/colaboracion_002.jpg'} btn_txt={'DONAR INSUMOS'}/>
-                    <Card source={'/assets/Colaboracion/colaboracion_003.jpg'} btn_txt={'DONAR SERVICIOS'}/>
+                    <Card source={'/assets/Colaboracion/colaboracion_001.jpg'} btn_txt={'DONAR DINERO'} onButtonClick={() => openModal('donar_dinero')} />
+                    <Card source={'/assets/Colaboracion/colaboracion_002.jpg'} btn_txt={'DONAR INSUMOS'} onButtonClick={() => openModal('donar_insumos')} />
+                    <Card source={'/assets/Colaboracion/colaboracion_003.jpg'} btn_txt={'DONAR SERVICIOS'} onButtonClick={() => openModal('donar_servicios')} />
                 </div>
             </section>
 
@@ -26,13 +50,16 @@ function Colaboracion() {
                     <h1 className='font-bold heading-1 text-left'>TIENDAS QUE COLABORAN</h1>
                     <p className='block lg:hidden font-medium par-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt labore minima modi quos reprehenderit delectus molestias dolor laborum impedit a?</p>
                     <div className='grid overflow-x-scroll grid-cols-1 lg:grid-cols-4 md:grid-cols-2 px-12 lg:px-0 snap-mandatory snap-x no-scrollbar gap-8' role='list'>
-                        <Image src={'/assets/Colaboracion/logo_001.png'} alt='Logo Tailwind' width={100} height={100} className='w-full' role='listItem'/>
-                        <Image src={'/assets/Colaboracion/logo_002.png'} alt='Logo Next' width={100} height={100} className='w-full' role='listItem'/>
-                        <Image src={'/assets/Colaboracion/logo_001.png'} alt='Logo Tailwind' width={100} height={100} className='w-full' role='listItem'/>
-                        <Image src={'/assets/Colaboracion/logo_002.png'} alt='Logo Next' width={100} height={100} className='w-full' role='listItem'/>
+                        <Image src={'/assets/Colaboracion/logo_001.png'} alt='Logo Tailwind' width={100} height={100} className='w-full' role='listItem' />
+                        <Image src={'/assets/Colaboracion/logo_002.png'} alt='Logo Next' width={100} height={100} className='w-full' role='listItem' />
+                        <Image src={'/assets/Colaboracion/logo_001.png'} alt='Logo Tailwind' width={100} height={100} className='w-full' role='listItem' />
+                        <Image src={'/assets/Colaboracion/logo_002.png'} alt='Logo Next' width={100} height={100} className='w-full' role='listItem' />
                     </div>
                 </div>
             </section>
+
+                <Modal isOpenD={modalVisible.donar_dinero } isOpenI={modalVisible.donar_insumos}  isOpenS={modalVisible.donar_servicios} onClose={closeModal}/>
+        
         </main>
     )
 }
