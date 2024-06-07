338ab722bd4ddd75c34221d4092d893db8a511a9
melani-depetris <melani.md98@gmail.com>
2024-06-07 19:35:59 -0300

    add modals

---
+++ src/app/(main)/colaboracion/components/Modal.jsx
@@ -0,0 +1,35 @@
+import DonaDineroContent from "./donaDineroContent";
+import DonaInsumosContent from "./DonaInsumosContent.jsx";
+import DonaServiciosContent from "./DonaServiciosContent.jsx";
+
+function Modal({ isOpenD, isOpenI, isOpenS, onClose }) {
+  if (!isOpenD && !isOpenI && !isOpenS) return null;
+
+  return (
+    <div
+      className="absolute h-full inset-0 flex justify-center items-center bg-black bg-opacity-50 backdrop-blur-sm z-50"
+      role="section"
+    >
+      <div
+        className="relative max-w-[90vw] top-[20vh] mx-auto lg:gap-14 px-12 py-10 lg:p-10 bg-white rounded-3xl overflow-y-clip"
+        role="contentInfo"
+      >
+        <button
+          className="absolute top-0 right-0 mr-6 mt-4 text-black"
+          onClick={onClose}
+        >
+          ×
+        </button>
+
+        {isOpenD && <DonaDineroContent />}
+
+        {isOpenI && <DonaInsumosContent />}
+
+        {isOpenS && <DonaServiciosContent />}
+        
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
