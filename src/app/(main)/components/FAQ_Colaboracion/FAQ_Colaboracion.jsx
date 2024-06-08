030976b168fed9dbffeee2c50bb1f4e4577f9589
FausUCU <88690038+FausUCU@users.noreply.github.com>
2024-06-08 14:56:04 -0300

    Ajuste Faqs
    Ajuste te transcion en FAqs

--- src/app/(main)/components/FAQ_Colaboracion/FAQ_Colaboracion.jsx
+++ src/app/(main)/components/FAQ_Colaboracion/FAQ_Colaboracion.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 function FAQ_Colaboracion() {
     return (
-        <div className="container w-full lg:w-4/5 mx-auto p-4 lg:p-2 font-[Poppins] max-w-screen-2xl mx-auto">
+        <div className="container w-full lg:w-4/5 mx-auto p-4 lg:p-2 font-[Poppins] max-w-screen-2xl">
 
             <div className="text-left mb-4">
                 <h2 className="text-lg text-primaryFont">Colaboración</h2>
