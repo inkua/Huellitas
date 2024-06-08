b0c8f5f1aeb1bcf5046c1d11db63254ac428a3ad
FausUCU <88690038+FausUCU@users.noreply.github.com>
2024-06-08 14:45:04 -0300

    Ajuste botones sumate
    Ajuste de los botones en componenete sumate a nuevo estilo

--- src/app/(main)/components/Sumate/Sumate.jsx
+++ src/app/(main)/components/Sumate/Sumate.jsx
@@ -6,11 +6,11 @@ function Sumate() {
             
             <div className="container w-5/5 lg:w-4/5 relative  max-w-screen-2xl mx-auto h-full" >
 
-                <div className="text-white px-4 py-4 rounded-lg mb-6 text-left lg:text-justify relative top-4 lg:top-11 ">
+                <div className="text-white px-4 py-4 rounded-lg mb-6 text-left lg:text-justify relative top-4 lg:top-11 lg:ml-4 ">
                     <p className="text-h2-m lg:text-h1-w font-semibold font-[Poppins] ">TU TAMBIÉN PUEDES AYUDAR</p>
                 </div>
 
-                <div className="flex-1 absolute invisible lg:visible w-[50%] top-10 bottom-0 right-0">
+                <div className="flex-1 absolute invisible lg:visible w-[50%] top-10 bottom-0 -right-24">
                     <Image src={'/assets/Sumate/girlDogDesktop.png'} 
                         className="w-fit h-fit"
                         width={500} 
@@ -31,14 +31,14 @@ function Sumate() {
                     <div className="flex-1 ">
 
                         <div className="w-full px-6 py-4 rounded-lg mb-6 hidden lg:grid  lg:grid-cols-1 lg:grid-rows-1 relative top-3">
-                            <input type="text" placeholder="Escribe tu nombre completo" className="w-5/12  px-4 py-2 bg-white rounded-full border border-gray-300 mb-4 placeholder-gray-500 text-primaryFont" />
+                            <input type="text" placeholder="Escribe tu nombre completo" className="w-6/12  px-4 py-2 bg-white rounded-full border border-gray-300 mb-4 placeholder-gray-500 text-primaryFont" />
                             <div>
-                                <input type="text" placeholder="Danos un número para contactarte" className="w-3/12  px-4 py-2 bg-white rounded-full border border-gray-300 mr-2 placeholder-gray-500 text-primaryFont" />
-                                <input type="text" placeholder="Dejanos tu mail" className="w-3/12 px-4 py-2 bg-white rounded-full border border-gray-300 mr-2 placeholder-gray-500 text-primaryFont grow " />
+                                <input type="text" placeholder="Danos un número para contactarte" className="w-4/12  px-4 py-2 bg-white rounded-full border border-gray-300 mr-2 placeholder-gray-500 text-primaryFont" />
+                                <input type="text" placeholder="Dejanos tu mail" className="w-4/12 px-4 py-2 bg-white rounded-full border border-gray-300 mr-2 placeholder-gray-500 text-primaryFont grow " />
                             </div>
-                            <div className='py-4'>
-                                <button className="w-fit h-7  bg-transparent px-3  border-white border-2 text-white rounded-full text-center  font-[Poppins] text-p3-m mr-3"> QUIERO SER VOLUNTARIO</button>
-                                <button className="w-fit h-7  bg-transparent px-3 border-white border-2 text-white rounded-full text-center font-[Poppins] text-p3-m"> QUIERO SER PADRINO</button>
+                            <div className='py-4 mt-3 mb-6'>
+                                <button className="shadow-xl px-6 w-fit h-8  bg-transparent   border-white border-2 text-white rounded-full text-center  font-[Poppins] text-p3-m mr-3"> QUIERO SER VOLUNTARIO</button>
+                                <button className="shadow-xl px-6 w-fit h-8  bg-transparent px-3 border-white border-2 text-white rounded-full text-center font-[Poppins] text-p3-m"> QUIERO SER PADRINO</button>
                             </div>
                         </div>
 
