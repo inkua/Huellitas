0326e202e5dae2f1a0c7202418265ee825cbb7cc
Melani-Depetris <melani.md98@gmail.com>
2024-06-10 18:03:24 -0300

    Melani (#25)
    * add modal Terms And Conditions
    
    * add responsive design
    
    * add components view fundation, component nuestraHistoria y Mision
    
    * change design footer, navBar, nuestraHistoria, mision
    
    * fix responsive navBar
    
    * fix footer w-full and add links
    
    * fix design
    
    * add modals
    
    * add design navBar
    
    * change local images by url to cloudinary
    
    * change local images Home by url to cloudinary
    
    * change local images Fundacion by url to cloudinary
    
    * change local images Colaboracion by url to cloudinary
    
    * change local images Adopcion by url to cloudinary
    
    * change local banner images by url to cloudinary and leave only one color palette
    
    * change local banner images by url to cloudinary and leave only one color palette

--- src/app/(main)/components/Vision/Vision.jsx
+++ src/app/(main)/components/Vision/Vision.jsx
@@ -1,9 +1,9 @@
 export const Vision = () => {
     return (
         <div className="flex flex-col mx-auto py-8 bg-tertiaryColor">
-            <div className="flex flex-col w-[80%] mx-auto py-8">
-                <h3 className="text-primaryFont font-bold text-h1-m mb-6" id="Vision">VISIÓN</h3>
-                <p className="text-primaryFont text-p1-m1">
+            <div className="flex flex-col w-[80%] mx-auto py-4 md:py-8">
+                <h3 className="text-primaryFont font-bold mb-6 heading-1" id="Vision">VISIÓN</h3>
+                <p className="text-primaryFont font-normal par-3">
                 Queremos ser reconocidos por nuestra capacidad para transformar vidas, tanto de los animales rescatados como de las personas involucradas en nuestra misión, inspirando un cambio positivo en la sociedad.  Buscamos impactar positivamente en nuestra comunidad, fomentando una cultura de compasión y empatía hacia los animales, donde cada individuo se sienta responsable de su bienestar.
                 </p>
             </div>
