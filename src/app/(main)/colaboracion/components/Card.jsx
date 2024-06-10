164fd491cc77c8f465e653bb4d027bc090bd724f
melani-depetris <melani.md98@gmail.com>
2024-06-10 17:28:11 -0300

    change local images Colaboracion by url to cloudinary

--- src/app/(main)/colaboracion/components/Card.jsx
+++ src/app/(main)/colaboracion/components/Card.jsx
@@ -5,7 +5,7 @@ function Card({source, btn_txt, onButtonClick }){
         <div role="listItem">
             <div className="flex flex-col gap-4 items-center lg:gap-8">
                 <div className='w-full overflow-hidden'>
-                    <Image src={source} alt='Foto del perrito' width={100} height={100} className='flex w-full' />
+                    <Image src={source} alt='Foto del perrito' width={700} height={700} className='flex w-full' />
                 </div>
                 <button type="button" className="w-full primary-btn" onClick={onButtonClick}>{btn_txt}</button>
             </div>
