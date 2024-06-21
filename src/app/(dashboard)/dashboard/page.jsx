dccd9651c01a157cd1e6d4b4e2c333a3df1df37b
Dev-Santi <bauzalarrosasantiago@gmail.com>
2024-06-21 02:00:12 -0300

    Crud de historias completo

--- src/app/(dashboard)/dashboard/page.jsx
+++ src/app/(dashboard)/dashboard/page.jsx
@@ -1,9 +1,18 @@
-function Dashboard() {
-    return (
-        <div>
-            <h1 className="bg-black">Historias</h1>
-        </div>
-    );
+import CRUD from "@/services";
+import Table from "./components/Table/Table";
+
+async function Dashboard() {
+    try {
+        const stories = await CRUD.getStories();
+        return (
+            <div>
+                <Table data={stories} />
+            </div>
+        );
+    } catch (e) {
+        console.log(e);
+        return <h2>Ha ocurrido un error al cargar las historias.</h2>;
+    }
 }
 
 export default Dashboard;
--- src/services/index.js
+++ src/services/index.js
@@ -1,29 +1,63 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default class CRUD {
-    static async getPets() {
+    static async getPosts() {
         try {
-            const response = await axios.get('http://localhost:1337/api/posts/');
+            const response = await axios.get(
+                "http://localhost:1337/api/posts/"
+            );
             return response.data.data;
         } catch (error) {
             console.log(error);
         }
     }
 
-    static async createPet(formData) {
+    static async getStories() {
         try {
-            await axios.post('http://localhost:1337/api/posts/', {
-                data: {
-                    title: formData.title,
-                    description: formData.description,
-                },
-            });
+            const response = await axios.get(
+                "http://localhost:1337/api/stories?populate=*"
+            );
+            return response.data.data;
         } catch (error) {
             console.log(error);
         }
     }
 
-    static async removePet() {}
+    static async createStory(item, image) {
+        try {
+            let formData = new FormData();
+            formData.append("files", image);
 
-    static async updatePet() {}
+            const newImage = await axios.post(
+                "http://localhost:1337/api/upload",
+                formData
+            );
+            item.data.image = newImage.data[0].id;
+
+            await axios.post("http://localhost:1337/api/stories/", item);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async updateStory(id, item) {
+        try {
+            await axios.put("http://localhost:1337/api/stories/" + id, item);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async removeStory(item) {
+        try {
+            await axios.delete("http://localhost:1337/api/stories/" + item.id);
+
+            await axios.delete(
+                "http://localhost:1337/api/upload/files/" +
+                    item.attributes.image.data.id
+            );
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
