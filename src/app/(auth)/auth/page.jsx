fff2ddc6e65dc83fab4d0a9d022e71039474b5cf
Dev-Santi <bauzalarrosasantiago@gmail.com>
2024-06-22 16:44:19 -0300

    working on Login page

--- src/app/(auth)/auth/page.jsx
+++ src/app/(auth)/auth/page.jsx
@@ -1,10 +1,9 @@
-
 function Login() {
     return (
         <body>
             <h1 className="bg-black text-lg">Login</h1>
         </body>
-    )
+    );
 }
 
-export default Login
+export default Login;
