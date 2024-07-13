e6becf437d40cead1bca0e27fedfc1bd7806542a
Gonzalo Vega <gevegaecheverria@gmail.com>
2024-07-13 20:42:31 -0300

    Text update, slider and accordion fix (#34)
    * Secciones Hero e Historias
    
    * Banner and media queries modifications
    
    * Adopcion page added. Changes to the Hero and History section, such as elements and responsive design
    
    * Custom class for text and button added
    
    * Fix padding primary-btn
    
    * Important plugin added
    
    * Responsive design modifications
    
    * Adoption page and responsive design finished
    
    * Link to adoption page added
    
    * components folder name correction
    
    * Correction in the stories section and automation of the adoption section
    
    * Collaboration page design finished
    
    * Fix responsive adoption card
    
    * Added button transition. Updated adoption page card
    
    * Slider finished
    
    * Fix padding slider
    
    * Slider Optimization
    
    * Accordion component
    
    * Added accordion
    
    * Text update
    
    * Slider and Accordion fix

--- src/app/(main)/components/History/History.jsx
+++ src/app/(main)/components/History/History.jsx
@@ -23,15 +23,15 @@ let items = [
         id: 4,
         par: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea cumque ad iure quaerat totam?',
         btn: 'SABER MAS...',
-        source: 'https://res.cloudinary.com/dorljfo6v/image/upload/v1717957502/huellitas/home-siguesushistorias-1_aezlpd.png',
+        source: 'https://res.cloudinary.com/dorljfo6v/image/upload/v1717957511/huellitas/home-siguesushistorias-2_ishlvs.png',
     },
 ]
 
 function History() {
     return (
-        <section className='flex flex-col w-full max-w-screen-2xl mx-auto justify-center items-center gap-8 py-16 bg-white'>
+        <section className='flex flex-col w-full max-w-screen-2xl mx-auto justify-center items-center gap-8 py-8 md:py-10 lg:py-16 bg-white'>
             <h1 className='w-[90%] lg:w-9/12 font-bold heading-1 text-left'>SIGUE SUS HISTORIAS</h1>
-            <Slider items={items} type={0} def={1.1} md={2.15} lg={3.15}></Slider>
+            <Slider items={items} type={0} def={1} md={2} lg={3}></Slider>
         </section>
 
     )
--- src/app/(main)/components/Slider/Slider.jsx
+++ src/app/(main)/components/Slider/Slider.jsx
@@ -1,21 +1,29 @@
 'use client'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation } from 'swiper/modules'
 import CardC from '../../colaboracion/components/Card'
 import CardH from '../History/Card'
 import CardColab from '../../colaboracion/components/Colab'
-import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import 'swiper/css/bundle'
 import 'swiper/css'
+import './custom-swiper-bullet.css'
 
 function Slider({ items, type, def, lg, md }) {
     return (
-        <Swiper slidesPerView={def} breakpoints={{ 1024: { slidesPerView: lg }, 768: { slidesPerView: md } }} spaceBetween={15} className='w-full lg:w-9/12 cursor-grab !px-[5%] lg:!px-0' >
-                {
-                    items.map(item => (
-                        <SwiperSlide key={item.id}>
-                            {createItem(type, item.source, item.par, item.btn, item.action)}
-                        </SwiperSlide>
-                    ))
-                }
+        <Swiper modules={[Navigation]}
+            navigation={true}
+            slidesPerView={def} breakpoints={{ 1024: { slidesPerView: lg }, 768: { slidesPerView: md } }}
+            spaceBetween={15}
+            loop={true}
+            className='w-full lg:w-9/12 cursor-grab !px-[5%] lg:!px-0' >
+            {
+                items.map(item => (
+                    <SwiperSlide key={item.id}>
+                        {createItem(type, item.source, item.par, item.btn, item.action)}
+                    </SwiperSlide>
+                ))
+            }
         </Swiper>
     )
 }
