735564ad72701957264b2dd856bfacc9cf7866dc
Gonzalo Vega <gevegaecheverria@gmail.com>
2024-07-14 16:04:38 -0300

    Added navbar animation. (#36)

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -2,8 +2,6 @@ import Hero from "./components/Hero/Hero";
 import History from "./components/History/History";
 import Sumate from "./components/Sumate/Sumate";
 import FAQ_Adopcion from "./components/FAQ_Adopcion/FAQ_Adopcion";
-import FAQ_Colaboracion from "./components/FAQ_Colaboracion/FAQ_Colaboracion";
-
 
 export default function Home() {
     return (
