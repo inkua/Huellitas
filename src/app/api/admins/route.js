c9d331a0dd7f20a69eb900ce30ba2d8625e589d2
Dev-Santi <bauzalarrosasantiago@gmail.com>
2024-07-18 17:01:02 -0300

    users crud and modals design

---
+++ src/app/api/admins/route.js
@@ -0,0 +1,56 @@
+import {
+    getRef,
+    formatData,
+    addElement,
+    putElement,
+    delElement,
+    delImage,
+} from "../../../services/services";
+
+export async function GET() {
+    try {
+        const ref = await getRef("admins");
+        return Response.json(formatData(ref));
+    } catch (e) {
+        return Response.json({ msg: e });
+    }
+}
+
+export async function POST(req) {
+    try {
+        const { token, data } = await req.json();
+        // Use token to validate request
+
+        const res = await addElement(data, "admins");
+        return Response.json({ status: 200, data: res });
+    } catch (e) {
+        console.log(e);
+        return Response.json({ status: 403, data: e });
+    }
+}
+
+export async function PUT(req) {
+    try {
+        const { token, id, data } = await req.json();
+        // Use token to validate request
+
+        const res = await putElement(data, id, "admins");
+        return Response.json({ status: 200, data: res });
+    } catch (e) {
+        console.log(e);
+        return Response.json({ status: 403, data: e });
+    }
+}
+
+export async function DELETE(req) {
+    try {
+        const { token, item } = await req.json();
+        // Use token to validate request
+
+        const res = await delElement(item.id, "admins");
+        return Response.json({ status: 200, data: res });
+    } catch (e) {
+        console.log(e);
+        return Response.json({ status: 403, data: e });
+    }
+}
