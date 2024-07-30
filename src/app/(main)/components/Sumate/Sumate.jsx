68482c881226f4a88eb13bf7a235d76a059499a9
FausUCU <88690038+FausUCU@users.noreply.github.com>
2024-07-30 18:05:14 -0300

    Ajuste de alertas a sumate

--- src/app/(main)/components/Sumate/Sumate.jsx
+++ src/app/(main)/components/Sumate/Sumate.jsx
@@ -1,25 +1,18 @@
 "use client";
 import React, { useState } from 'react';
 function Sumate() {
-    const [email, setEmail] = useState('');
-    const [name, setName] = useState('');
-    const [phone, setPhone] = useState('');
-
-    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [phone, setPhone] = useState("");
 
     const handleSubmit = (e) => {
-        e.preventDefault();
-        setIsSubmitted(true);
-
-        const form = e.target;
-        if (form.checkValidity()) {
-            console.log('Form submitted');
-        } else {
-            console.log('Form has errors');
-        }
+        e.preventDefault()
+        alert(`
+            email:${email}
+            name:${name}
+            phone:${phone}
+            `)
     };
-
-    
 
     return (
         <section className="flex justify-center w-full bg-primaryColor pt-8 md:pt-10 lg:pt-16 xs:bg-sample bg-1 bg-no-repeat bg-right-bottom md:bg-none bg-none">
@@ -30,7 +23,7 @@ function Sumate() {
                     <h2 className="heading-1 font-bold !text-white">TÚ TAMBIÉN PUEDES AYUDAR</h2>
                     <div className="flex items-center w-full">
 
-                        <form noValidate onSubmit={handleSubmit} className="flex-1 mt-4 lg:py-4">
+                        <form onSubmit={handleSubmit} className="flex-1 mt-4 lg:py-4">
                             <div className="w-full  rounded-lg mb-6  grid  grid-cols-1 grid-rows-1 relative top-3">
                                 <div className='mb-4'>
                                         <input
@@ -38,11 +31,11 @@ function Sumate() {
                                             placeholder="Nombre completo"
                                             value={name}
                                             onChange={(e) => setName(e.target.value)}
-                                            className={`w-8/12 sm:w-6/12 p-2 bg-white rounded-full border ${isSubmitted && name.trim().length < 2 ? 'border-red-500' : 'border-gray-300'} placeholder-gray-500 text-primaryFont par-3`}
-                                            required
+                                            className={`w-8/12 sm:w-6/12 p-2 bg-white rounded-full border placeholder-gray-500 text-primaryFont par-3`}
+                                            required={true}
                                             minLength="2"
                                         />
-                                        {isSubmitted && name.trim().length < 2 && <p className="text-red-500 text-sm md:mt-2">Nombre no válido</p>}
+                                    
                                     </div>
                                     <div className='mb-[13px] md:mb-4 flex flex-col lg:flex-row gap-4 md:gap-0'>
                                     <div className='w-6/12 sm:w-4/12'>
@@ -51,22 +44,23 @@ function Sumate() {
                                             placeholder="Celular"
                                             value={phone}
                                             onChange={(e) => setPhone(e.target.value)}
-                                            className={`p-2 bg-white rounded-full border ${isSubmitted && !phone.match(/^[0-9\s\+\-\(\)]{7,}$/) ? 'border-red-500' : 'border-gray-300'} mr-2 md:mr-0 placeholder-gray-500 text-primaryFont par-3`}
-                                            required
-                                            pattern="[0-9\s\+\-\(\)]{7,}"
+                                            className={`p-2 bg-white rounded-full border 'border-gray-300' mr-2 md:mr-0 placeholder-gray-500 text-primaryFont par-3`}
+                                            required={true}
+                                            pattern="[0-9\s\+\-\(\)]{10,}"
                                         />
-                                        {isSubmitted && !phone.match(/^[0-9\s\+\-\(\)]{7,}$/) && <p className="text-red-500 text-sm mt-1 md:mt-2">Número de teléfono no válido</p>}
+                                        
                                     </div>
                                     <div className='w-6/12 sm:w-4/12'>
                                         <input
                                             type="email"
                                             placeholder="Correo"
+                                            pattern="[^\s@]+@[^\s@]+.[^\s@]+"
                                             value={email}
                                             onChange={(e) => setEmail(e.target.value)}
-                                            className={`p-2 bg-white rounded-full border ${isSubmitted && !email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/) ? 'border-red-500' : 'border-gray-300'} mr-2 md:mr-0 placeholder-gray-500 text-primaryFont par-3`}
-                                            required
+                                            className={`p-2 bg-white rounded-full border 'border-gray-300' mr-2 md:mr-0 placeholder-gray-500 text-primaryFont par-3`}
+                                            required={true}
                                         />
-                                        {isSubmitted && !email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/) && <p className="text-red-500 text-sm mt-1 md:mt-2">Correo electrónico no válido</p>}
+                                        
                                     </div>
                                 </div>  
 
