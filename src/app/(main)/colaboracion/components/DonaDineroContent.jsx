530483c5f0eaee8078d030e365f8cd34756a88cf
gevega <gevegaecheverria@gmail.com>
2024-07-30 17:25:54 -0300

    Update button style and padrino section commented

--- src/app/(main)/colaboracion/components/DonaDineroContent.jsx
+++ src/app/(main)/colaboracion/components/DonaDineroContent.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-const DonaDineroContent = ({onClose}) => {
+const DonaDineroContent = ({ onClose }) => {
   return (
     (
       <div className="w-[80%] md:w-[70%] lg:w-[35%] max-h-[95%] bg-white rounded-3xl overflow-y-clip p-4 relative" role="contentInfo">
@@ -10,7 +10,7 @@ const DonaDineroContent = ({onClose}) => {
             <p className="par-3 justify-center text-center">Elegí el monto que puedas donar y haz una transfrencia a cualquiera de las siguientes cuentas:</p>
           </div>
 
-          <div className="flex flex-col items-center w-full pb-6 border-b border-black gap-6">
+          <div className="flex flex-col items-center w-full gap-6">
             <div className="flex items-center flex-row">
               <div className="flex items-center flex-row">
                 <div className="w-[50%] flex justify-left">
@@ -65,7 +65,7 @@ const DonaDineroContent = ({onClose}) => {
                   <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">  <path fill="black" d="M21 8.94a1.31 1.31 0 0 0-.06-.27v-.09a1.07 1.07 0 0 0-.19-.28l-6-6a1.07 1.07 0 0 0-.28-.19a.32.32 0 0 0-.09 0a.88.88 0 0 0-.33-.11H10a3 3 0 0 0-3 3v1H6a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3v-1h1a3 3 0 0 0 3-3V9zm-6-3.53L17.59 8H16a1 1 0 0 1-1-1ZM15 19a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1h1v7a3 3 0 0 0 3 3h5Zm4-4a1 1 0 0 1-1 1h-8a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h3v3a3 3 0 0 0 3 3h3Z"></path></svg>
                 </button>
                 <div className="flex flex-col">
-                <p className="par-3 flex">CA 08500009646</p>
+                  <p className="par-3 flex">CA 08500009646</p>
                 </div>
               </div>
             </div>
@@ -75,12 +75,13 @@ const DonaDineroContent = ({onClose}) => {
             </div>
           </div>
         </div>
-        <div className="w-full text-center pt-6 flex flex-col justify-center items-center gap-2">
-          <h2 className="par-1 font-semibold">PADRINO</h2>
-          <p className="par-3">Si querés donar todos los meses, haz click en el siguiente enlace:</p>
-          <button type="button" className="w-fit primary-btn">QUIERO SER PADRINO</button>
-
-        </div>
+        {/* 
+          <div className="w-full text-center pt-6 flex flex-col justify-center items-center gap-2">
+            <h2 className="par-1 font-semibold">PADRINO</h2>
+            <p className="par-3">Si querés donar todos los meses, haz click en el siguiente enlace:</p>
+            <button type="button" className="w-fit primary-btn">QUIERO SER PADRINO</button>
+          </div>
+        */}
         <div className="absolute top-2 right-4 heading-2 cursor-pointer hover:text-primaryColor duration-200" onClick={onClose}>✖</div>
       </div>
 
