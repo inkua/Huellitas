7f3104ab6f0aad61102b8267f36216baf00acbf6
gevega <gevegaecheverria@gmail.com>
2024-08-01 21:06:18 -0300

    Fix history section and adoption section

--- src/app/(main)/components/History/Card.jsx
+++ src/app/(main)/components/History/Card.jsx
@@ -1,10 +1,16 @@
 import Image from 'next/image'
 
-function Card({source, par, btn}) {
+function Card({ source, par, btn }) {
     return (
-        <div className='flex flex-col gap-4 snap-center !mb-0' role='listItem'>
-            <Image src={source} alt='Foto del perrito protagonista' width={700} height={700} className='w-full rounded-3xl !mb-0' loading="eager"/>
-            <p className='font-normal par-3'>{par}</p>
+        <div className='flex flex-col gap-4 snap-center !mb-0 justify-between !h-full' role='listItem'>
+            <div className='flex w-full h-full aspect-[16/9] overflow-hidden rounded-3xl relative'>
+                <Image src={source} alt='Foto del perrito protagonista' width={700} height={700} className='absolute w-full h-full object-cover' loading="eager" />
+            </div>
+            <p className='font-normal par-3 text-justify'>
+                {par &&
+                    par.substring(0, 100) + '...'
+                }
+            </p>
             <button className='w-fit secondary-btn'>{btn}</button>
         </div>
     )
