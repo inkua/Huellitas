e70c0a9a6643545627557e46a67194c1e1f04131
gevega <gevegaecheverria@gmail.com>
2024-08-04 10:26:41 -0300

    Fix modal

--- src/app/(main)/colaboracion/components/DonaServiciosContent.jsx
+++ src/app/(main)/colaboracion/components/DonaServiciosContent.jsx
@@ -1,21 +1,21 @@
-const DonaServiciosContent = ({onClose}) => {
+const DonaServiciosContent = ({ onClose }) => {
   return (
-    <div className="w-[90%] md:w-[80%] lg:w-[50%] max-h-[95%] p-6 md:p-12 lg:p-14 bg-white rounded-3xl relative flex flex-col justify-center items-center">
+    <div className="w-[90%] md:w-[80%] lg:w-[50%] max-h-[95%] p-6 md:p-12 lg:p-14 bg-white rounded-3xl relative flex flex-col justify-center items-center gap-8">
       <div className="">
         <h2 className="heading-2 font-semibold">ESTERILIZACIONES</h2>
-        <p className="par-3">
+        <p className="par-3 text-justify">
           Autorizamos atención médica en las veterinarias con las que tenemos convenio y así mismo esterilizamos.
         </p>
       </div>
-      <div className="rounded-lg p-2 py-8">
+      <div className="">
         <h2 className="heading-2 font-semibold">HOGAR DE PASO</h2>
-        <p className="par-3">
+        <p className="par-3 text-justify">
           También puedes unirte a nuestro grupo de voluntariado donde nos ayudarán en las diferentes actividades pro fondo que tengamos, también siendo hogar de paso.
         </p>
       </div>
-      <div className="flex flex-col justify-center items-center bg-primaryColor rounded-lg py-4 px-4 lg:px-20 gap-4">
+      <div className="flex flex-col gap-4 bg-primaryColor rounded-3xl py-4 px-4 lg:px-20 w-full">
         <h2 className="par-2 !text-white font-semibold w-full">¿CÓMO PUEDO PRESTAR LOS SERVICIOS?</h2>
-        <p className="par-3 !text-white">
+        <p className="par-3 !text-white text-justify">
           Otra forma es unirte a nuestro grupo de padrinos mensual, donde solo pedimos una donación de $10.000,00 por mes para poder cubrir las cuentas pendientes de este mismo.          </p>
       </div>
 
