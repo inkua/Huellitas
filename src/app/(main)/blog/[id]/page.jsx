11941156dee7829a82dc1bf05595fd9a1819ea04
Dev-Santi <bauzalarrosasantiago@gmail.com>
2024-08-10 22:11:22 -0300

    fixes + new features

--- src/app/(main)/blog/[id]/page.jsx
+++ src/app/(main)/blog/[id]/page.jsx
@@ -1,43 +1,48 @@
-import Image from "next/image"
-import History from "../../components/History/History"
+import Image from "next/image";
+import History from "../../components/History/History";
 
-import { getStoryById } from "@/services/services"
+import { getStoryById } from "@/services/services";
 
 async function Story({ params }) {
-    const { id } = params
+   const { id } = params;
 
-    const story = await getStoryById(id)
+   const story = await getStoryById(id);
 
-    return (
-        <main className="text-black p-4 w-full max-w-screen-2xl mx-auto h-full font-[Poppins]">
-            {story.data ?
+   return (
+      <main className="text-black p-4 w-full max-w-screen-2xl mx-auto h-full font-[Poppins]">
+         {story.data ? (
             <>
-                <section className="w-[90%] lg:w-9/12 mx-auto">
-                    <figure className="relative w-full aspect-video max-h-[320px] lg:max-h-[370px]">
-                        <Image 
+               <section className="w-[90%] lg:w-9/12 mx-auto">
+                  <figure className="relative w-full aspect-video max-h-[320px] lg:max-h-[370px]">
+                     <Image
                         className="rounded-3xl object-cover object-center mb-[11px]"
                         src={story.data.imagen}
                         fill
-                        alt="pet image"/>
-                    </figure>
-                    <span className="italic text-[11px] md:text-[20px]">{story.data.nombre}, Escrito por Chavely Lizcano Alvarez Huellitas Cartagena _ 30/7/2024</span>
-                </section>
+                        alt="pet image"
+                     />
+                  </figure>
+                  <span className="italic text-[11px] md:text-[20px]">
+                     {story.data.nombre + (story.data.fecha ? ", " + story.data.fecha : "")}
+                  </span>
+               </section>
 
-                <article id="blogArticle" className="w-[90%] lg:w-9/12 mx-auto mt-[27px]" dangerouslySetInnerHTML={{ __html: story.data.articulo }} >
-                    {/* se renderiza el articulo */}
-                </article>
+               <article
+                  id="blogArticle"
+                  className="w-[90%] lg:w-9/12 mx-auto mt-[27px]"
+                  dangerouslySetInnerHTML={{ __html: story.data.articulo }}
+               >
+                  {/* se renderiza el articulo */}
+               </article>
             </>
-            
-            :
-
+         ) : (
             <section className="w-[90%] lg:w-9/12 mx-auto">
-                <h1>Historia no encontrada {':('}</h1>
+               <h1>Historia no encontrada {":("}</h1>
             </section>
-            }
+         )}
 
-            <History />
-        </main>
-    )
+         <History />
+      </main>
+   );
 }
 
-export default Story
+export default Story;
