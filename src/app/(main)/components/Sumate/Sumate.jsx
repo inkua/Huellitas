34320cdcf845345ac0f70dfe4b12e93674de961b
FausUCU <88690038+FausUCU@users.noreply.github.com>
2024-08-13 13:44:08 -0300

    Faustino (#70)
    * Ajuste de colaboracion
    
    * ajuste de colaboracon
    
    cambio de logos y aguste de baner en colaboracion
    
    * Aligment adjust
    
    ajuste de el aligniamientos en pagina colaboracion
    
    * Ajuste ealiniamiento
    
    ajuste de aliniamiento en componente de colaboracion
    
    * Donacion ecnomica mobile
    
    ajuste de tarjeta de colaboracion economica en mobile
    
    * Donacion economica desktop
    
    tarjeta de donacion economica para desktop
    
    * slide fix
    
    * remove image test
    
    * creacion de planilla
    
    creacion de planilla en pagina de contacto
    
    * add a global.css
    
    agrego clase para slider 2 a css
    
    * forma en contacto
    
    * barra inferior
    
    creando barra inferior en contacto con direcciones
    
    * lower bar consultas
    
    barra inferio de consultas mobile
    
    * lower bar desktop
    
    bara blanca en contacto desktop
    
    * Update map.jsx
    
    * ajuste tarjeta
    
    ajuste tarjetas colaboracion
    
    * ajustes de tarjeta
    
    * add validation a contacto
    
    se agrego validacion a los campos de contacto, vlidando email, nombre y telefono
    
    * field check in main page
    
    checkea los valores de usario en la seccion de tu tambien puedes ayudar en la pagina principal
    
    * npm i
    
    * Ajuste de boton
    
    boton de contacto ajustado al estandar
    
    * ajuste forma consulta
    
    imputs de consulta validad desde usario
    
    * ajuste form sumate
    
    validacion de datos de la forma summate en la pagina principal
    
    * forma validacion summate
    
    * Ajuste de alertas a sumate
    
    * ajuste form contacto
    
    * Send email form contacto
    
    Se envia la informacion de consulta en email, la cuenta a utilizar deve ser configurada con password de aplicaciones
    
    * Forma de inicio
    
    ajuste e implmtnacion de forma con envio de email
    
    * Ajuste forma de contactor
    
    Agregar espacio a la fomra de contacto en mobiel entre los campos
    
    * Ajuste de formularios
    
    ajuste de los comprobante de email, eliminacion de datos inecesario y proteccion ante multiple emails
    
    * remove comments
    
    remove comments

--- src/app/(main)/components/Sumate/Sumate.jsx
+++ src/app/(main)/components/Sumate/Sumate.jsx
@@ -1,54 +1,83 @@
 "use client";
 import React, { useState } from 'react';
+
 function Sumate() {
     const [email, setEmail] = useState("");
     const [name, setName] = useState("");
     const [phone, setPhone] = useState("");
+    const [isSubmitting, setIsSubmitting] = useState(false); 
+    const [subject, setSubject] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        alert(`
-            email:${email}
-            name:${name}
-            phone:${phone}
-            `)
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        setIsSubmitting(true); 
+        setSubject(subject);
+
+        const form = e.target;
+        if (form.checkValidity()) {
+            try {
+                const response = await fetch('/api/send-email', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ name, phone, email, subject }),
+                });
+
+                if (response.ok) {
+                    console.log('Email sent successfully');
+                    alert('Solicitud enviada con éxito!');
+                    setEmail('');
+                    setName('');
+                    setPhone('');
+                    setSubject('');
+                } else {
+                    console.log('Failed to send email');
+                    alert('Error al enviar la solicitud.');
+                }
+            } catch (error) {
+                console.error('Error:', error);
+                alert('Error al enviar la solicitud.');
+            } finally {
+                setIsSubmitting(false); 
+            }
+        } else {
+            console.log('Form has errors');
+            setIsSubmitting(false); 
+        }
     };
 
     return (
         <section className="flex justify-center w-full bg-primaryColor pt-8 md:pt-10 lg:pt-16 xs:bg-sample bg-1 bg-no-repeat bg-right-bottom md:bg-none bg-none">
-
-            <div className="w-full max-w-screen-2xl mx-auto h-full" >
-
+            <div className="w-full max-w-screen-2xl mx-auto h-full">
                 <div className='w-[90%] lg:w-9/12 m-auto md:bg-sample pb-8 md:pb-10 lg:pb-16 bg-2 bg-no-repeat bg-right-bottom lg:bg-4'>
                     <h2 className="heading-1 font-bold !text-white">TÚ TAMBIÉN PUEDES AYUDAR</h2>
                     <div className="flex items-center w-full">
-
-                        <form onSubmit={handleSubmit} className="flex-1 mt-4 lg:py-4">
-                            <div className="w-full  rounded-lg mb-6  grid  grid-cols-1 grid-rows-1 relative top-3">
+                        <form className="flex-1 mt-4 lg:py-4" onSubmit={(e) => handleSubmit(e)}>
+                            <div className="w-full rounded-lg mb-6 grid grid-cols-1 grid-rows-1 relative top-3">
                                 <div className='mb-4'>
-                                        <input
-                                            type="text"
-                                            placeholder="Nombre completo"
-                                            value={name}
-                                            onChange={(e) => setName(e.target.value)}
-                                            className={`w-8/12 sm:w-6/12 p-2 bg-white rounded-full border placeholder-gray-500 text-primaryFont par-3`}
-                                            required={true}
-                                            minLength="2"
-                                        />
-                                    
-                                    </div>
-                                    <div className='mb-[13px] md:mb-4 flex flex-col lg:flex-row gap-4 md:gap-0'>
+                                    <input
+                                        type="text"
+                                        placeholder="Nombre completo"
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)}
+                                        className="w-8/12 sm:w-6/12 p-2 bg-white rounded-full border placeholder-gray-500 text-primaryFont par-3"
+                                        required={true}
+                                        minLength="2"
+                                    />
+                                </div>
+                                <div className='mb-[13px] md:mb-4 flex flex-col lg:flex-row gap-4 md:gap-0'>
                                     <div className='w-6/12 sm:w-4/12'>
                                         <input
                                             type="text"
                                             placeholder="Celular"
                                             value={phone}
                                             onChange={(e) => setPhone(e.target.value)}
-                                            className={`p-2 bg-white rounded-full border 'border-gray-300' mr-2 md:mr-0 placeholder-gray-500 text-primaryFont par-3`}
+                                            className="p-2 bg-white rounded-full border 'border-gray-300' mr-2 md:mr-0 placeholder-gray-500 text-primaryFont par-3"
                                             required={true}
                                             pattern="[0-9\s\+\-\(\)]{10,}"
                                         />
-                                        
                                     </div>
                                     <div className='w-6/12 sm:w-4/12'>
                                         <input
@@ -57,24 +86,31 @@ function Sumate() {
                                             pattern="[^\s@]+@[^\s@]+.[^\s@]+"
                                             value={email}
                                             onChange={(e) => setEmail(e.target.value)}
-                                            className={`p-2 bg-white rounded-full border 'border-gray-300' mr-2 md:mr-0 placeholder-gray-500 text-primaryFont par-3`}
+                                            className="p-2 bg-white rounded-full border 'border-gray-300' mr-2 md:mr-0 placeholder-gray-500 text-primaryFont par-3"
                                             required={true}
                                         />
-                                        
                                     </div>
-                                </div>  
-
+                                </div>
                                 <div className='md:mt-3 flex flex-col-reverse md:flex-row gap-2 md:gap-[2%] mt-2'>
-                                    <button type="submit" className="secondary-btn !text-white !border-white w-fit">QUIERO SER VOLUNTARIO</button>
-                                    <button type="submit" className="secondary-btn !text-white !border-white w-fit">QUIERO SER PADRINO</button>
+                                    <button
+                                        onClick={() => setSubject("Quiero ser voluntario")}
+                                        className="secondary-btn !text-white !border-white w-fit"
+                                        disabled={isSubmitting} 
+                                    >
+                                        QUIERO SER VOLUNTARIO
+                                    </button>
+                                    <button
+                                        onClick={() => setSubject("Quiero ser padrino")}
+                                        className="secondary-btn !text-white !border-white w-fit"
+                                        disabled={isSubmitting} 
+                                    >
+                                        QUIERO SER PADRINO
+                                    </button>
                                 </div>
                             </div>
                         </form>
-
                     </div>
-
                 </div>
-
             </div>
         </section>
     );
