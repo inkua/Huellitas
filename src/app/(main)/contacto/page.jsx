9d6deced887ba1137d4289d0f4f7cfdf4e1a5b22
gevega <gevegaecheverria@gmail.com>
2024-08-14 15:13:05 -0300

    Fix contact grid

--- src/app/(main)/contacto/page.jsx
+++ src/app/(main)/contacto/page.jsx
@@ -8,7 +8,35 @@ export const metadata = {
 function Contacto() {
     return (
         <main className="">
-            <Map />
+            <section className="w-full">
+                <Map />
+            </section>
+            <section className="w-full">
+                <div className="w-full h-full mx-auto max-w-screen-2xl flex justify-center py-14 md:py-18 lg:py-22">
+                    <div className="w-[75%] flex flex-col gap-6">
+                        <div>
+                            <p className='text-primaryFont text-p3-m text-center mb-2'>Para llegar a más personas y poder seguir ayudando</p>
+                            <h1 className='par-1 !font-bold text-center'>¡NO TE OLVIDES DE SEGUIRNOS EN NUESTRAS REDES!</h1>
+                        </div>
+
+                        <div className="flex justify-center">
+                            <div className="flex flex-col md:flex-row md:justify-center w-[80%] gap-2 md:gap-8">
+
+                                <div className="flex items-center justify-left">
+                                    <div className="bg-ig-mobile bg-no-repeat bg-contain w-[24px] h-[24px] md:w-[35px] md:h-[35px]"></div>
+                                    <a className=" !text-grayFont par-3 ml-2 hover:!text-gray-800 duration-150" href="https://www.instagram.com/huellitas.ctgna/" target="_blank" rel="noreferrer">@huellitas.ctgna</a>
+                                </div>
+
+                                <div className="flex items-center justify-left">
+                                    <div className="bg-tk-mobile bg-no-repeat bg-contain w-[24px] h-[24px] md:w-[35px] md:h-[35px]"></div>
+                                    <a className=" !text-grayFont par-3 ml-2 hover:!text-gray-800 duration-150" href="https://www.tiktok.com/@huellitascartagena" target="_blank" rel="noreferrer" >@huellitascartagena</a>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
     )
 }
